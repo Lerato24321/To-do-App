7ac7ac7fccd7d5a8d284193e42dd7ceec63c4f02
Lerato24321 <123421998+Lerato24321@users.noreply.github.com>
2024-06-08 10:20:08 +0200

    added the checked and delete functionality in JavaScript

--- src/java.js
+++ src/java.js
@@ -1,28 +1,28 @@
-
+const enterTask = document.getElementById("input-area");
+const taskList = document.getElementById("task-list");
 
 function createTask() {
-
-
-  if (enterTask.value === '') {
-    alert("Task cannot be blank ☹️");
+  if (enterTask.value === "") {
+    alert("Task cannot be blank ☹️ !");
   } else {
     let li = document.createElement("li");
     li.innerHTML = enterTask.value;
     taskList.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = " \u00d7";
+    li.appendChild(span);
   }
-
-
+  enterTask.value = "";
 }
 
-
-
-
-
-const enterTask = document.getElementById("input-area");
-const taskList = document.getElementById("task-list");
-
-
-
-
-
-
+taskList.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("marked");
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+    }
+  },
+  false
+);
