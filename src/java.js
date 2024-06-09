a57b328b83061c449ac54bd4bc41c43bbabe7f6b
Lerato24321 <123421998+Lerato24321@users.noreply.github.com>
2024-06-09 07:45:33 +0200

    Added storing data on reload functionality in JavaScript

--- src/java.js
+++ src/java.js
@@ -13,6 +13,7 @@ function createTask() {
     li.appendChild(span);
   }
   enterTask.value = "";
+  saveTask();
 }
 
 taskList.addEventListener(
@@ -20,9 +21,20 @@ taskList.addEventListener(
   function (e) {
     if (e.target.tagName === "LI") {
       e.target.classList.toggle("marked");
+      saveTask();
     } else if (e.target.tagName === "SPAN") {
       e.target.parentElement.remove();
+      saveTask();
     }
   },
   false
 );
+
+
+function saveTask() {
+  localStorage.setItem ("data", taskList.innerHTML);
+}
+function displayTask() {
+  taskList.innerHTML = localStorage.getItem ("data");
+}
+displayTask();
